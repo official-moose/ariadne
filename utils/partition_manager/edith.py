2ceb8ad8dab53a99cdb40e81b3d19bcf339fae51
root <root@srv681942.hstgr.cloud>
2025-09-23 18:39:36 +0000

    Auto-sync: 2025-09-23 18:39:36

--- utils/partition_manager/edith.py
+++ utils/partition_manager/edith.py
@@ -201,7 +201,7 @@ def drop_old_signals_partitions(cur) -> int:
         SELECT 
             child.relname AS partition_name,
             pg_get_expr(child.relpartbound, child.oid) AS partition_range
-        FROM pg_inherits
+        FROM pg_inherits 
         JOIN pg_class parent ON pg_inherits.inhparent = parent.oid
         JOIN pg_class child ON pg_inherits.inhrelid = child.oid
         WHERE parent.relname = 'signals_intel'
